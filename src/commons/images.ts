34436770ddea66da79f63dc37ae8cada90af2925
AbdelrahmanHs86 <bodi25sameh@gmail.com>
2022-09-30 21:30:55 +0300

    feat: Import assets

---
+++ src/commons/images.ts
@@ -0,0 +1,29 @@
+import approve from 'assets/Actions/approve.png';
+import cancel from 'assets/Actions/cancel.png';
+import remove from 'assets/Actions/delete.png';
+import options from 'assets/Actions/options.png';
+
+import like from 'assets/Reactions/likes.png';
+import heart from 'assets/Reactions/heart.png';
+import comments from 'assets/Reactions/comments.png';
+import share from 'assets/Reactions/shares.png';
+import shareT from 'assets/Reactions/shares-t.png';
+import views from 'assets/Reactions/views.png';
+
+import facebookLogo from 'assets/Logos/Facebook.png';
+import twitterLogo from 'assets/Logos/Twitter.png';
+
+export {
+  approve,
+  cancel,
+  remove,
+  options,
+  like,
+  heart,
+  comments,
+  share,
+  shareT,
+  views,
+  facebookLogo,
+  twitterLogo,
+};
