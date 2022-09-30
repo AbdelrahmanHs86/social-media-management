e7f5c624b180f0eb691d4326b535e029137562d9
AbdelrahmanHs86 <bodi25sameh@gmail.com>
2022-09-30 21:42:21 +0300

    feat: Create Cards component

---
+++ src/features/Posts/Cards/Cards.tsx
@@ -0,0 +1,77 @@
+import React, { useEffect } from 'react';
+import { getPosts } from 'store/Slices/PostsSlice';
+import { useAppDispatch, useAppSelector } from 'store/hooks';
+import Reactions from './Reactions/Reactions';
+import Actions from './Actions/Actions';
+import PostStatus from './PostStatus/PostStatus';
+import moment from 'moment';
+import { Post } from 'commons/models';
+
+const Cards: React.FC = () => {
+  const posts = useAppSelector((state) => state.postsReducer);
+  const dispatch = useAppDispatch();
+
+  const handleDate = (date: string) => {
+    const [dateValues, timeValues] = date.split(' ');
+    // const [year, month, day] = dateValues.split('-');
+    const [hour, minute] = timeValues.split(':');
+    let newDate = '';
+    if (hour === '00' && minute === '00') {
+      newDate = moment(date).format('DD MMMM YYYY');
+    }
+    // const newDate = new Date(+year, +month - 1, +day, +hour, +minute);
+    // console.log(newDate);
+    else {
+      newDate = moment(date).format('DD MMMM YYYY - HH:mm');
+    }
+
+    // const newDate = DateTime.fromSQL(date).toFormat();
+    return newDate;
+  };
+
+  const postList = () => {
+    const data: JSX.Element[] = [];
+    for (const [key, value] of Object.entries(posts.postList)) {
+      // map.set(key, value);
+
+      if (value instanceof Array) {
+        const data2 = value.map((post: Post, index: number) => (
+          <div key={index}>
+            <PostStatus
+              link={post.account.link}
+              channel={post.account.channel}
+            />
+            <div>
+              <li>{handleDate(post.published_at)}</li>
+              <Actions status={post.status} />
+              <div>{post.entry.message}</div>
+              <img src={post.entry.image[0]} />
+              <Reactions channel={post.account.channel} />
+            </div>
+          </div>
+        ));
+
+        data.unshift(
+          <ul>
+            {moment(key).format('DD MMMM YYYY')}
+            {data2}
+          </ul>
+        );
+      }
+    }
+    return data;
+  };
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [dispatch]);
+
+  return (
+    <>
+      <div>Cards</div>
+      {postList()}
+    </>
+  );
+};
+
+export default Cards;
