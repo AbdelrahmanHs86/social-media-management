e51dbe3be5a40094d249b1c8450de112ba5e93ca
AbdelrahmanHs86 <bodi25sameh@gmail.com>
2022-09-30 21:40:02 +0300

    feat: Create postsSlice with getPosts action

--- src/store/Slices/PostsSlice.ts
+++ src/store/Slices/PostsSlice.ts
@@ -1,16 +1,53 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk, PayloadAction } from '@reduxjs/toolkit';
+import data from '../../assets/data.json';
+import { PostDates, PostsState } from 'commons/models';
 
-// interface Post {
-//   id: number;
-// }
+const initialState: PostsState = {
+  postList: [],
+  error: {},
+};
 
-// interface PostsState {
-//   posts: Post[];
-// }
+export const getPosts = createAsyncThunk(
+  'posts/getPosts',
+  async (arg, thunkAPI) => {
+    const { rejectWithValue } = thunkAPI;
+
+    try {
+      // const res = await fetch(
+      //   'https://api.jsonbin.io/v3/b/62ed1523e13e6063dc6cd5ee', ////// Exausted Requests
+      //   {
+      //     method: 'GET',
+      //     headers: {
+      //       'X-ACCESS-KEY':
+      //         '$2b$10$smpejxmp4gmvulslxzJAs.gQ4v4PXntI/WeSCOOFXAd3KZl7LFJcW',
+      //     },
+      //   }
+      // );
+      // const data = await res.json();
+      // return data?.record?.posts_by_date;
+      return data.posts_by_date;
+    } catch (error) {
+      return rejectWithValue(error);
+    }
+  }
+);
 
 const PostsSlice = createSlice({
   name: 'posts',
-  initialState: { posts: [] },
+  initialState,
   reducers: {},
-  extraReducers: {},
+  extraReducers: {
+    //getPosts
+    [getPosts.pending.type]: (state) => {
+      console.log();
+    },
+    [getPosts.fulfilled.type]: (state, action: PayloadAction<PostDates[]>) => {
+      state.postList = action.payload;
+    },
+    [getPosts.rejected.type]: (state, action: PayloadAction) => {
+      state.error = action.payload;
+    },
+  },
 });
+
+export default PostsSlice.reducer;
